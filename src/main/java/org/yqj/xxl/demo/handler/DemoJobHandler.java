3bbf98a5f39d87f5127cf8975e1a8ac5d4ecf3ee
qijunyao <qijunyao@xiaohongshu.com>
2019-11-19 16:43:26 +0800

    init config all condition

--- src/main/java/org/yqj/xxl/demo/handler/DemoJobHandler.java
+++ src/main/java/org/yqj/xxl/demo/handler/DemoJobHandler.java
@@ -3,7 +3,6 @@ package org.yqj.xxl.demo.handler;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
-import com.xxl.job.core.log.XxlJobLogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
